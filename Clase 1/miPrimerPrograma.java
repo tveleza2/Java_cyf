2bdc426ddd45f8ff565b3432eb5131e5dafeb3a7
tveleza2 <93617802+tveleza2@users.noreply.github.com>
2024-10-02 18:07:40 -0500

    Excersie 1 added

---
+++ Clase 1/miPrimerPrograma.java
@@ -0,0 +1,9 @@
+public class miPrimerPrograma {
+    public static void main(String[] args) {
+        String yourName = "Tomas";
+        int age = 22;
+        System.out.println("Bienvenido a tu curso de JAVA");
+        System.out.println("Hola mi nombre es "+ yourName);
+        System.out.println("Mi edad es " + age);
+    }
+}
--- hello_world.java
+++
@@ -1,5 +0,0 @@
-public class hello_world {
-    public static void main(String[] args){
-        System.out.print("Hello world");
-    }
-}
