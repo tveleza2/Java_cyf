976254ce47829aea190900af7b355bd1d259d34a
tveleza2 <93617802+tveleza2@users.noreply.github.com>
2024-10-02 18:32:42 -0500

    Complementary excercise

---
+++ Clase 1/complementariaC1.java
@@ -0,0 +1,10 @@
+public class complementariaC1 {
+    public static void main(String[] args) {
+        String mensajeBienvenida = "Bienvenido a Java";
+        float temperaturaActual = 18.6f;
+        boolean haceFrio = true;
+        System.out.println(mensajeBienvenida+", la temperatura actual es "+temperaturaActual);
+        System.out.println("¡¿HACE FRÍO?! "+haceFrio);
+
+    }
+}
