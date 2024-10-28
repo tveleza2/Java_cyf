308cc63266e1c1b70fbe55faed75af05c89f78f8
tveleza2 <93617802+tveleza2@users.noreply.github.com>
2024-10-28 09:43:37 -0500

    Class 14 excercises added.

--- Clase_14/countWhiteSpace.java
+++ Clase_14/countWhiteSpace.java
@@ -1,5 +1,23 @@
 package Clase_14;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class countWhiteSpace {
-    
+    public static void main(String[] args) {
+        System.out.print("\033\143"); // Limpia la consola
+        System.out.print("Escriba una cadena: ");
+        int countWhiteSpace = 0;
+        try (Scanner tSc = new Scanner(System.in)){
+            String myStr = tSc.nextLine();
+            for(int i=0;i<myStr.length();i++){
+                if(Character.isWhitespace(myStr.charAt(i))){
+                    countWhiteSpace ++;
+                }
+            } 
+            System.out.printf("La cadena contiene %d espacios en blanco. \n",countWhiteSpace);
+        }catch (InputMismatchException e) {
+            System.out.println("Debe escribir una cadena.");
+        }
+    }
 }
--- Clase_14/str2int.java
+++ Clase_14/str2int.java
@@ -1,5 +1,17 @@
 package Clase_14;
 
+import java.util.Scanner;
+
 public class str2int {
-    
+    public static void main(String[] args) {
+        System.out.print("\033\143"); // Limpia la consola
+        System.out.print("Escriba un número entero: ");
+        try (Scanner tSc = new Scanner(System.in)){
+            String myStr = tSc.nextLine();
+            int myInt = Integer.valueOf(myStr); 
+            System.out.printf("El número ingresado es: %d. \n",myInt);
+        }catch (NumberFormatException e) {
+            System.out.println("Debe escribir un número.");
+        }
+    }
 }
